9f9d402da89a8f84c84a20e80facc6d7f5427f97
Daniel V. <daniel.viddy@gmail.com>
2025-04-13 11:08:58 -0400

    Module 6 work

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -29,7 +29,53 @@ const tripsFindByCode = async(req, res) => {
     }
 };
 
+
+const tripsAddTrip = async(req, res) => {
+
+    const q = await Model.create({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    }).then((result)=>{
+        return res.status(201).json(result);
+    }).catch((err)=>{
+        return res.status(400).json(err);
+    });
+};
+
+const tripsUpdateTrip = async(req, res)=> {
+
+    console.log(req.params);
+    console.log(req.body);
+
+    const q = await Model.findOneAndUpdate(
+
+        {'code': req.params.tripCode},
+        {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        }
+    ).exec().then((result)=>{
+        return res.status(201).json(result);
+    }).catch((err)=>{
+        return res.status(400).json(err);
+    });
+};
+
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
